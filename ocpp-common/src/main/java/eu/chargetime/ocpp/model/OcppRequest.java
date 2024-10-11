324c52719e94d8c6b06c2630f8329a5bcbf86e12
Daniel Vocke <danielvocke@gmail.com>
2024-10-11 06:19:16 -0700

    reformatted the code

--- ocpp-common/src/main/java/eu/chargetime/ocpp/model/OcppRequest.java
+++ ocpp-common/src/main/java/eu/chargetime/ocpp/model/OcppRequest.java
@@ -23,24 +23,18 @@ public abstract class OcppRequest implements Request {
 	}
 
 	@Override
-	public String getRequestId(){
-		// if requestId already set, get from here
-		if(requestId != null){
-			return requestId;
+	public String getRequestId() {
+		if (requestId == null && rawMessageJson != null) {
+			/*
+			 * a raw message JSON has this format: [2,"THE_ID_WE_CARE_ABOUT",StatusNotification,{}].
+			 * This is a quick and dirty way to extract the ID without parsing the whose JSON message.
+			 */
+			String[] messageParts = rawMessageJson.split("\"");
+			if (messageParts.length > 1) {
+				requestId = messageParts[1];
+			}
 		}
-
-		try {
-			// id needs to be in the 2nd position of the array after a first item that's not a string.
-			// this split logic won't work if the first item is also a string
-			String requestId = rawMessageJson.split("\"")[1];
-
-			// set requestId before returning it so that next time, it's simply returned from this getter
-			this.requestId = requestId;
-			return requestId;
-		} catch (NullPointerException | IndexOutOfBoundsException exception){
-			// only catch and return null for these 2 exception types, let everything else throw the exception
-			return null;
-		}
+		return requestId;
 	}
 
 }
